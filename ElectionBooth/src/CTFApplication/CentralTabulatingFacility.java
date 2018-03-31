94572a687e4177a8529082f177a3d02ba94159b8
DeonBotelho <deon.botelho@gmail.com>
2018-03-31 00:29:00 -0400

    BasicClientGUI
    Generated JFrame client gui application with limited actions.

---
+++ ElectionBooth/src/CTFApplication/CentralTabulatingFacility.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package COE817.CTFApplication;
+
+/**
+ *
+ * @author Deon
+ */
+public class CentralTabulatingFacility {
+    
+}
---
+++ ElectionBooth/src/ClientApplication/ClientController.java
@@ -0,0 +1,49 @@
+package COE817.ClientApplication;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Deon
+ */
+public final class ClientController {
+
+    //Test
+    public static void main(String[] args) {
+        ClientController x = new ClientController();
+        
+    }
+
+    ClientGUI gui;
+
+    public ClientController() {
+        initGUI();
+        gui.resetGUI();
+    }
+
+    public void initGUI() {
+        JFrame frame = new JFrame();
+        ActionListener buttonEvent = (ActionEvent e) -> {
+            register();
+        };        
+        gui = new ClientGUI(buttonEvent);
+        frame.add(gui);
+        frame.setSize(new Dimension(350, 350));
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        gui.resetGUI();
+        
+
+    }
+
+    public void register() {
+        System.out.println(gui.getFirstName());
+        System.out.println(gui.getLastName());
+        System.out.println(gui.getSIN());
+    }
+
+}
